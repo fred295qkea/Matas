bbe3b5660843adead5feb3937ecfb91b92947fc6
”Frederik <fred295q@stud.kea.dk>
2023-11-16 15:28:33 +0100

    merge & fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    extend: {
 
+    
     colors:{
       "matas": {
         500: "#002643",
@@ -25,14 +27,9 @@ module.exports = {
       }
 
     },
-
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
+
+
   },
   plugins: [],
 }
