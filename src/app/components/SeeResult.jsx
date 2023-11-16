1edd7c12b4fafe64a2380c07f31a7ea05daad5b6
sylvester <sylledejlig@gmail.com>
2023-11-16 15:22:28 +0100

    2 components

---
+++ src/app/components/SeeResult.jsx
@@ -0,0 +1,10 @@
+export default function SeeResult() {
+    return(
+        <div className="flex items-center justify-center min-h-screen bg-white">
+            <div className="grid grid-rows-3 max-w-xs gap-10">
+                <h2 className="row-start-1 items-center text-center text-mantas-500 font-bold text-4xl ">Så har vi fundet den perfekte duft til dig</h2> 
+            <button className="row-start-2 row-end-4 rounded-full h-80 lg:h-96 w-80 lg:w-96 bg-mantas-300 button-color hover:bg-mantas-400 hover:shadow-lg transform hover:scale-105 transition-transform duration-300"><p className="font-bold text-2xl">TRYK OG SE</p></button>
+           </div>
+        </div>
+    )
+}
