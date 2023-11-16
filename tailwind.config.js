84402766f13aa1e488dd2754a9987f411c440e25
”Frederik <fred295q@stud.kea.dk>
2023-11-16 09:54:23 +0100

    Tailwind colors and reset

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,26 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+
+    colors:{
+      "matas": {
+        500: "#002643",
+        400: "#284461",
+        300: "#556882",
+        200: "#8893A8",
+        100: "#DEE1E9"
+      },
+
+      "mantas": {
+        500: "#00395B",
+        400: "#2774AE",
+        300: "#44BDEE",
+        200: "#81B0D2",
+        100: "#CDDCED"
+      }
+
+    },
+
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
