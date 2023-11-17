9a4579dab1a6b5256254734e25dcd389e0a9bba8
sylvester <sylledejlig@gmail.com>
2023-11-17 13:15:56 +0100

    more styling and also some animation

--- src/app/components/Result.jsx
+++ src/app/components/Result.jsx
@@ -1,12 +1,12 @@
 export default function Result() {
     return(
         <div className="flex items-center justify-center min-h-screen bg-mantas-300">
-            <div className=" grid grid-row-4 lg:rounded-full lg:bg-matas-100 full-circle"  >
-                <div className="flex flex-wrap justify-center my-12">
+            <div className=" grid grid-row-4 lg:rounded-full lg:bg-white full-circle"  >
+                <div className="flex flex-col items-center justify-center ">
                     <div className="img-size bg-matas-500"><p className="flex justify-center">IMAGE HERE</p></div>
-                    <p className="">PROPS PROPS PROPS OR SMTH</p>
+                    <p className=""> BRAND - PERFUME</p>
                 </div>
-                <div>
+                <div className="flex justify-center">
                     <button>Læg i kurv</button> 
                     <button>Læs mere</button>
                 </div>
--- src/app/components/SeeResult.jsx
+++ src/app/components/SeeResult.jsx
@@ -1,9 +1,23 @@
+"use client"
+import React, {useState} from "react"
+
+
+
 export default function SeeResult() {
+    const [effect, setEffect] = useState(false);
     return(
         <div className="flex items-center justify-center min-h-screen bg-white">
             <div className="grid grid-rows-3 max-w-xs gap-10">
                 <h2 className="row-start-1 items-center text-center text-mantas-500 font-bold text-4xl ">Så har vi fundet den perfekte duft til dig</h2> 
-            <button className="row-start-2 row-end-4 rounded-full h-80 lg:h-96 w-80 lg:w-96 bg-mantas-300 button-color hover:bg-mantas-400 hover:shadow-lg transform hover:scale-105 transition-transform duration-300"><p className="font-bold text-2xl">TRYK OG SE</p></button>
+            <button
+          className={`${ effect && "btn-animation"} row-start-2 row-end-4 rounded-full h-80 lg:h-96 w-80 lg:w-96 font-bold text-4xl bg-mantas-300 button-color hover:bg-mantas-400 hover:shadow-lg transform hover:scale-105 transition-transform duration-300`}
+          onClick={() => {
+            setEffect(true);
+          }}
+          onAnimationEnd={() => setEffect(false)} // kan jeg putte min nextstep her
+        >
+          SMELL ME NOW!
+        </button>
            </div>
         </div>
     )
